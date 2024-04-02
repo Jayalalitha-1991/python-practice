ce455cc4982e0edd29352609946e3ca4e64193ce
Jaya <jayallaitha.maridi@gmail.com>
2024-04-02 15:25:45 +0530

    Area

---
+++ area.py
@@ -0,0 +1,10 @@
+def cal_rect_area(l,w):
+    area = l*w
+    return area
+
+l = float(input("Enter the length of the rect angle: "))
+w = float(input("Enter the widith of the rect angle: "))
+
+a = cal_rect_area(l,w)
+
+print("The are of the rectangle is:",a)
---
+++ twinkle.py
@@ -0,0 +1,16 @@
+import sys
+import datetime
+
+print ("Twinkle, twinkle, little star,\n\tH ow I wonder what you are! \n\t Up above the world so high,\n\tlike a diamond in the sky. \n\tTwinkle, twinkle, little star, How I wonder what you are")
+
+print(sys.version)
+
+v = input("SOme comma seperated values:")
+
+list = v.split(',')
+
+tuple = tuple(list)
+
+print("list:",list)
+
+print("tuple:",tuple)
